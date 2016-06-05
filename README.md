d222f5055a2986ae893b65a4636f4c58533563cb
Louis Pilfold <louis@lpil.uk>
2016-06-05 15:25:50 +0100

    Innit, with tests

---
+++ README.md
@@ -0,0 +1,9 @@
+lisp
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ test/lisp_test.erl
@@ -0,0 +1,5 @@
+-module(lisp_test).
+-include_lib("eunit/include/eunit.hrl").
+
+suite_test() ->
+  ?assert(true).
